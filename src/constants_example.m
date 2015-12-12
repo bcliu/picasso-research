3e29e95258514e78165d3ae635604fc6cd8195d0
Chen Liu <chenliu@cmu.edu>
2015-12-12 17:28:30 -0500

    Initial

---
+++ src/constants_example.m
@@ -0,0 +1,2 @@
+caffe_root = '/mnt/software/caffe/';
+research_root = '/mnt/software/research/';
---
+++ src/featureExtraction.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make sure that caffe is on the python path:
+caffe_root = '/mnt/software/caffe/'
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+def vis_square(data, padsize=1, padval=0):
+    data -= data.min()
+    data /= data.max()
+    
+    # force the number of filters to be square
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = ((0, n ** 2 - data.shape[0]), (0, padsize), (0, padsize)) + ((0, 0),) * (data.ndim - 3)
+    data = np.pad(data, padding, mode='constant', constant_values=(padval, padval))
+    
+    # tile the filters into an image
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    
+    plt.imshow(data)
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+caffe.set_mode_cpu()
+net = caffe.Net(caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt',
+                caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel',
+                caffe.TEST)
+
+# input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2,0,1))
+transformer.set_mean('data', np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1)) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))
+
+net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image('/mnt/software/research/eye1.jpg'))#caffe_root + 'examples/images/cat.jpg'))
+out = net.forward()
+
+print("Predicted class is #{}.".format(out['prob'][0].argmax()))
+
+plt.imshow(transformer.deprocess('data', net.blobs['data'].data[0]))
+
+imagenet_labels_filename = caffe_root + 'examples/ilsvrc12/synset_words.txt'
+labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
+
+# sort top k predictions from softmax output
+top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
+print labels[top_k]
+
+# filters = net.params['conv1'][0].data
+# vis_square(filters.transpose(0, 2, 3, 1))
+
+feat = net.blobs['conv5'].data[0]
+vis_square(feat, padval=0.5)
