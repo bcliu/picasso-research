3332959fb295d01aa6078df5f16371ce4e542bc1
VM Chen <chenliu@cmu.edu>
2015-12-21 00:37:40 -0500

    More pictures for svm

--- src/svm.py
+++ src/svm.py
@@ -2,14 +2,14 @@ from constants import *
 import argparse
 from os import walk
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 parser = argparse.ArgumentParser(description='LOL')
 parser.add_argument('--type1')
 parser.add_argument('--type2')
 parser.add_argument('--others')
 args = parser.parse_args()
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 # Make sure that caffe is on the python path:
 import sys
@@ -38,41 +38,38 @@ transformer.set_channel_swap('data', (2,1,0))
 datapoints = []
 datalabels = []
 
-for (dirpath, dirnames, filenames) in walk(args.type1):
-    for filename in filenames:
-        path = dirpath + "/" + filename
+pairs = [(args.type1, 0), (args.type2, 1), (args.others, 2)]
 
-        net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-        out = net.forward()
+for pair in pairs:
+    for (dirpath, dirnames, filenames) in walk(pair[0]):
+        for filename in filenames:
+            path = dirpath + filename
 
-        datapoints.append(net.blobs['fc6'].data[0].tolist())
-        datalabels.append(0)
+            net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
+            out = net.forward()
 
-for (dirpath, dirnames, filenames) in walk(args.type2):
-    for filename in filenames:
-        path = dirpath + "/" + filename
+            datapoints.append(net.blobs['fc6'].data[0].tolist())
+            datalabels.append(pair[1])
 
-        net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-        out = net.forward()
-
-        datapoints.append(net.blobs['fc6'].data[0].tolist())
-        datalabels.append(1)
-
-for (dirpath, dirnames, filenames) in walk(args.others):
-    for filename in filenames:
-        path = dirpath + "/" + filename
-
-        net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-        out = net.forward()
-
-        datapoints.append(net.blobs['fc6'].data[0].tolist())
-        datalabels.append(2)
+            print 'Processed ' + path + ' as type ' + str(pair[1])
 
 from sklearn import svm
 clf = svm.SVC()
 clf.fit(datapoints, datalabels)
 
-net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image('/mnt/software/research/images/eyes/cubism/cubism-eye.jpg-sm.jpg'))
-out = net.forward()
+num_data = 0
+num_correctly_classified = 0
 
-clf.predict([net.blobs['fc6'].data[0].tolist()])
+for pair in pairs:
+    for (dirpath, dirnames, filenames) in walk(pair[0]):
+        for filename in filenames:
+            path = dirpath + filename
+            num_data = num_data + 1
+
+            net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
+            out = net.forward()
+            prediction = clf.predict([net.blobs['fc6'].data[0].tolist()])
+            if prediction[0].astype('str') == str(pair[1]):
+                num_correctly_classified = num_correctly_classified + 1
+
+print "Out of " + str(num_data) + " training examples, " + str(num_correctly_classified) + " were correctly classified"
