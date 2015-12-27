6f3ef0052575e834fb254b6ec0451fbcf35cc502
Chen Liu <chenliu@cmu.edu>
2015-12-27 10:33:41 -0500

    Updated svm code

--- src/svm.py
+++ src/svm.py
@@ -4,12 +4,14 @@ from os import walk
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--type1')
-parser.add_argument('--type2')
-parser.add_argument('--others')
-#parser.add_argument('--save-variables', required=False)
+parser.add_argument('--type1', default=research_root + 'images/eyes/normal/sm/', required=False)
+parser.add_argument('--type2', default=research_root + 'images/noses/normal/sm/', required=False)
+parser.add_argument('--others', default=research_root + 'images/others/sm/', required=False)
+parser.add_argument('--dump', help='dump variables to files for fast loading', action='store_true')
+parser.add_argument('--loaddump', help='load dumped variables', action='store_true')
 args = parser.parse_args()
 
 # Make sure that caffe is on the python path:
@@ -17,6 +19,7 @@ import sys
 sys.path.insert(0, caffe_root + 'python')
 
 import caffe
+from sklearn import svm
 
 imagenet_labels_filename = caffe_root + 'data/ilsvrc12/synset_words.txt'
 labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
@@ -38,25 +41,39 @@ transformer.set_channel_swap('data', (2,1,0))
 
 datapoints = []
 datalabels = []
+clf = svm.SVC()
 
 pairs = [(args.type1, 1), (args.type2, 2), (args.others, 3)]
 
-for pair in pairs:
-    for (dirpath, dirnames, filenames) in walk(pair[0]):
-        for filename in filenames:
-            path = dirpath + filename
+def load_image(path):
+    net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
+    out = net.forward()
 
-            net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-            out = net.forward()
+def predict():
+    return clf.predict([net.blobs['fc6'].data[0].tolist()])
 
-            datapoints.append(net.blobs['fc6'].data[0].tolist())
-            datalabels.append(pair[1])
+if args.loaddump == False:
 
-            print 'Processed ' + path + ' as type ' + str(pair[1])
+    for pair in pairs:
+        for (dirpath, dirnames, filenames) in walk(pair[0]):
+            for filename in filenames:
+                path = dirpath + filename
+                load_image(path)
+                datapoints.append(net.blobs['fc6'].data[0].tolist())
+                datalabels.append(pair[1])
 
-from sklearn import svm
-clf = svm.SVC()
-clf.fit(datapoints, datalabels)
+                print 'Processed ' + path + ' as type ' + str(pair[1])
+
+    clf.fit(datapoints, datalabels)
+
+    if args.dump:
+        f = open('svm-data.dump', 'wb')
+        pickle.dump([datapoints, datalabels, clf], f)
+        f.close()
+else:
+    f = open('svm-data.dump')
+    datapoints, datalabels, clf = pickle.load(f)
+    f.close()
 
 num_data = 0
 num_correctly_classified = 0
@@ -66,10 +83,8 @@ for pair in pairs:
         for filename in filenames:
             path = dirpath + filename
             num_data = num_data + 1
-
-            net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-            out = net.forward()
-            prediction = clf.predict([net.blobs['fc6'].data[0].tolist()])
+            load_image(path)
+            prediction = predict()
             if prediction[0].astype('str') == str(pair[1]):
                 num_correctly_classified = num_correctly_classified + 1
 
