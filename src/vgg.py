69319a989989418f58da9fba8ee93beefbd25d2f
Chen Liu <chenliu@cmu.edu>
2016-03-18 14:47:15 -0400

    Loading dump and auto generate plots

--- src/vgg.py
+++ src/vgg.py
@@ -1,6 +1,3 @@
-# VARIABLES PICKLED: [args, net, sample_mask, vectors, vec_origin_file, vec_location, n_clusters, kmeans_obj, predicted]
-
-
 from constants import *
 import argparse
 from os import walk
@@ -8,16 +5,35 @@ import os
 
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
+#import matplotlib.pyplot as plt
 import pickle
 
-plt.rcParams['figure.figsize'] = (10, 10)
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+import get_receptive_field as rf
+
+from matplotlib.patches import Rectangle
 
 # Make sure that caffe is on the python path:
 import sys
 sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+
+# Find better way to write it to distribute more evenly
+def sample(width, height, number):
+    mat = [[False for x in range(width)] for x in range(width)]
+    prob_true = number * 1.0 / width / height
+    for x in range(width):
+        for y in range(height):
+            mat[y][x] = True if np.random.random() < prob_true else False
+
+    return mat
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--images', default=research_root + 'images/flickr/eyes-yes/', required=False)
@@ -27,12 +43,22 @@ parser.add_argument('--n_clusters', default=32, required=False)
 parser.add_argument('--center_only_path', default=None, required=False)
 parser.add_argument('--center_only_neuron_x', default=None, required=False)
 parser.add_argument('--gpu', default=0, required=False)
+parser.add_argument('--load_dump_from', default=None, required=False)
+parser.add_argument('--save_dump_to', default=None, required=False)
+parser.add_argument('--save_plots_to', default=None, required=False)
+
 args = parser.parse_args()
+
+if args.save_plots_to is not None:
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
 
 print 'Loading images from ' + args.images
 print 'Sampling ' + str(args.sample_fraction) + ' of responses from layer ' + args.layer
-
-import caffe
 
 imagenet_labels_filename = caffe_root + 'data/ilsvrc12/synset_words.txt'
 labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
@@ -42,10 +68,6 @@ if mode == 'cpu':
     caffe.set_mode_cpu()
 else:
     caffe.set_mode_gpu()
-
-#net = caffe.Net(caffe_root + 'models/vgg16/VGG_ILSVRC_16_layers_deploy.prototxt',
-#                caffe_root + 'models/vgg16/VGG_ILSVRC_16_layers.caffemodel',
-#                caffe.TEST)
 
 #transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 #transformer.set_transpose('data', (2,0,1))
@@ -66,24 +88,11 @@ net.blobs['data'].reshape(1, 3, 224, 224)
 
 def load_image(path, echo=True):
     net.predict([caffe.io.load_image(path)], oversample=False)
-    #net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
-    #out = net.forward()
     #if echo:
     #    print("Predicted class is #{}.".format(out['prob'][0].argmax()))
 
     #    top_k = net.blobs['prob'].data[0].flatten().argsort()[-1:-6:-1]
     #    print labels[top_k]
-
-
-# Find better way to write it to distribute more evenly
-def sample(width, height, number):
-    mat = [[False for x in range(width)] for x in range(width)]
-    prob_true = number * 1.0 / width / height
-    for x in range(width):
-        for y in range(height):
-            mat[y][x] = True if np.random.random() < prob_true else False
-
-    return mat
 
 
 ## NOTE: Resize every image to 224x224, or resize but keep original ratio? Does it affect things?
@@ -98,67 +107,80 @@ vec_origin_file = []
 # Array of (arrays of (x, y))
 vec_location = []
 
-# Loop through every image in the given directory
-for (dirpath, dirnames, filenames) in walk(args.images):
-    for filename in filenames:
-        path = os.path.abspath(os.path.join(dirpath, filename))
-        print 'Processed', path
-        load_image(path, False)
-
-        response = net.blobs[args.layer].data[0]
-        num_responses = len(response)
-        height_response = len(response[0])
-        width_response = len(response[0][0])
-
-        if len(sample_mask) == 0:
-            # sample_mask not initialized yet; sample new
-            print str(num_responses) + ' filters of ' + str(height_response) + 'x' + str(width_response)
-
-            sample_mask = sample(width_response, height_response, float(args.sample_fraction) * width_response * height_response)
-
-        for y in range(height_response):
-            for x in range(width_response):
-                if sample_mask[y][x]:
-                    ## NOTE: DOUBLE CHECK IF FIRST IS Y SECOND IS X, corresponding to images
-                    vectors.append(response[:, y, x].copy())
-                    vec_origin_file.append(path)
-                    vec_location.append((x, y))
-
-
-print 'Got', len(vectors), 'vectors randomly sampled'
-# Load the images of which only the center patches will be used
-if args.center_only_path is not None:
-    print 'Loading images of which center patches will be used'
-    location_to_pick = int(args.center_only_neuron_x)
-    print 'Center neuron x and y coordinate:', location_to_pick
-
-    for (dirpath, dirnames, filenames) in walk(args.center_only_path):
+if args.load_dump_from is not None:
+    print 'Loading dump file from', args.load_dump_from
+    f = open(args.load_dump_from)
+    args_dump, sample_mask, vectors, vec_origin_file, vec_location, n_clusters, kmeans_obj, predicted = pickle.load(f)
+    if hasattr(args, 'load_dump_from'):
+        args_dump.load_dump_from = args.load_dump_from
+    if hasattr(args, 'save_dump_to'):
+        args_dump.save_dump_to = args.save_dump_to
+    args = args_dump
+    f.close()
+    print 'Finished loading dump.'
+else:
+    # Loop through every image in the given directory
+    for (dirpath, dirnames, filenames) in walk(args.images):
         for filename in filenames:
             path = os.path.abspath(os.path.join(dirpath, filename))
             print 'Processed', path
             load_image(path, False)
 
             response = net.blobs[args.layer].data[0]
-            vectors.append(response[:, location_to_pick, location_to_pick].copy())
-            vec_origin_file.append(path)
-            vec_location.append((location_to_pick, location_to_pick))
+            num_responses = len(response)
+            height_response = len(response[0])
+            width_response = len(response[0][0])
+
+            if len(sample_mask) == 0:
+                # sample_mask not initialized yet; sample new
+                print str(num_responses) + ' filters of ' + str(height_response) + 'x' + str(width_response)
+
+                sample_mask = sample(width_response, height_response,
+                        float(args.sample_fraction) * width_response * height_response)
+
+            for y in range(height_response):
+                for x in range(width_response):
+                    if sample_mask[y][x]:
+                        ## NOTE: DOUBLE CHECK IF FIRST IS Y SECOND IS X, corresponding to images
+                        vectors.append(response[:, y, x].copy())
+                        vec_origin_file.append(path)
+                        vec_location.append((x, y))
+
+
+    print 'Got', len(vectors), 'vectors randomly sampled'
+    # Load the images of which only the center patches will be used
+    if args.center_only_path is not None:
+        print 'Loading images of which center patches will be used'
+        location_to_pick = int(args.center_only_neuron_x)
+        print 'Center neuron x and y coordinate:', location_to_pick
+
+        for (dirpath, dirnames, filenames) in walk(args.center_only_path):
+            for filename in filenames:
+                path = os.path.abspath(os.path.join(dirpath, filename))
+                print 'Processed', path
+                load_image(path, False)
+
+                response = net.blobs[args.layer].data[0]
+                vectors.append(response[:, location_to_pick, location_to_pick].copy())
+                vec_origin_file.append(path)
+                vec_location.append((location_to_pick, location_to_pick))
+
+    # TRY PCA, ICA AS WELL!!!
+    # if this method works, continue to explore how to make your initial method work as well
+
+    n_clusters = int(args.n_clusters)
+    n_restarts = 10
+    kmeans_obj = KMeans(init='k-means++', n_clusters=n_clusters, n_init=n_restarts)
+    predicted = kmeans_obj.fit_predict(vectors)
+
+    if args.save_dump_to is not None:
+        print 'Saving dump to', args.save_dump_to
+        f = open(args.save_dump_to, 'wb')
+        pickle.dump([args, sample_mask, vectors, vec_origin_file, vec_location, n_clusters, kmeans_obj, predicted], f)
+        f.close()
+        print 'Finished saving dump'
 
 print 'Got', len(vectors), 'vectors in total for clustering'
-
-# TRY PCA, ICA AS WELL!!!
-# if this method works, continue to explore how to make your initial method work as well
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
-
-import get_receptive_field as rf
-
-n_clusters = int(args.n_clusters)
-n_restarts = 10
-kmeans_obj = KMeans(init='k-means++', n_clusters=n_clusters, n_init=n_restarts)
-predicted = kmeans_obj.fit_predict(vectors)
 
 ##### PCA
 pca = PCA(n_components=80)
@@ -168,7 +190,6 @@ pca = PCA(n_components=80)
 
 
 #########
-from matplotlib.patches import Rectangle
 
 def get_top_n_in_cluster(cluster_i, n):
     scores = []
@@ -275,7 +296,8 @@ def view_nth_cluster(cluster_i, n):
 
     plt.show()
 
-def view_n_from_clusters(from_cluster, to_cluster, n_each):
+#### THIS FUNCTION IS TOO SLOW. CAN BE MUCH IMPROVED!!!
+def view_n_from_clusters(from_cluster, to_cluster, n_each, save_plots=False):
     fig = plt.figure()
     fig_id = 1
 
@@ -293,6 +315,23 @@ def view_n_from_clusters(from_cluster, to_cluster, n_each):
 
             plt.axis('off')
     
-    plt.show()
+    if save_plots:
+        plt.savefig(os.path.join(args.save_plots_to, args.layer + '_' + 'clusters' + str(from_cluster) + 'to' + str(to_cluster) + '.png'))
+    else:
+        plt.show()
 
-    return
+if args.save_plots_to is not None:
+    matplotlib.use('Agg')
+    if not os.path.exists(args.save_plots_to):
+        os.makedirs(args.save_plots_to)
+    # 8 clusters each time, 16 images for each cluster
+    num_rounds = n_clusters / 8
+    if n_clusters % 8 != 0:
+        num_rounds = num_rounds + 1
+
+    for i in range(num_rounds):
+        start = i * 8
+        end = start + 7
+        if end >= n_clusters:
+            end = n_clusters - 1
+        view_n_from_clusters(start, end, 16, True)
